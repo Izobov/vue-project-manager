6c474181042ded39bbe4d3565b24a94fbba8da4f
Ivan Izobov <izobov9@gmail.com>
2023-12-18 11:06:20 +0300

    [dev] Suite with Vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <Sidebar />
+    <MainContainer/>
+  </main>
+</template>
+
+<script>
+  import MainContainer from './components/MainContainer.vue';
+  import Sidebar from './components/Sidebar.vue';
+  import "@dhx/trial-suite/codebase/suite.min.css"
+
+  export default {
+    components: {
+      MainContainer,
+      Sidebar
+    }
+  }
+</script>
+
+<style scoped>
+  main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+  }
+</style>
+
+<style>
+  .bordered {
+    border: var(--dhx-border);
+  }
+</style>
